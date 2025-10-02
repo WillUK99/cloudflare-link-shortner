de80b4580dc48abd673571b04133afa150fb4daa
Joe <76099444+WillUK99@users.noreply.github.com>
2025-10-02 08:18:39 +0100

    feat: add ai destination check

--- apps/data-service/src/workflows/destination-evalutation-workflow.ts
+++ apps/data-service/src/workflows/destination-evalutation-workflow.ts
@@ -1,16 +1,30 @@
+import { aiDestinationChecker } from '@/helpers/ai-destination-checker';
+import { collectDestinationInfo } from '@/helpers/browser-render';
 import { WorkflowEntrypoint, type WorkflowEvent, type WorkflowStep } from 'cloudflare:workers';
 
 
-export class DestinationEvaluationWorkflow extends WorkflowEntrypoint<Env, unknown> {
-  async run(event: Readonly<WorkflowEvent<unknown>>, step: WorkflowStep) {
+
+export class DestinationEvaluationWorkflow extends WorkflowEntrypoint<Env, DestinationStatusEvaluationParams> {
+  async run(event: Readonly<WorkflowEvent<DestinationStatusEvaluationParams>>, step: WorkflowStep) {
 
     const collectedData = await step.do("Collect rendered destination page data", async () => {
-      console.log("Collecting rendered destination page data");
-      return {
-        "dummydata": "dummydata",
-      }
+      return collectDestinationInfo(this.env, event.payload.destinationUrl);
     });
 
+    const aiStatus = await step.do(
+      'Use AI to check status of page',
+      {
+        retries: {
+          limit: 0,
+          delay: 0,
+        },
+      },
+      async () => {
+        return await aiDestinationChecker(this.env, collectedData.bodyText);
+      },
+    );
+
     console.log(collectedData);
+    console.log(aiStatus);
   }
 }
